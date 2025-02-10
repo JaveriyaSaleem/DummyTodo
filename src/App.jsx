b198b6cc217885289fd99b66362f6763bd443d93
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-02-10 21:48:02 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 
 import axios from "axios";
-import Todo from "./Components/Todo";
-import { TodoProvider } from "./Context/context";
+
 
 function App() {
   return (
-    <TodoProvider>
-      <Todo />
-    </TodoProvider>
-  );
+<></>
+  )
 }
 
 export default App;
